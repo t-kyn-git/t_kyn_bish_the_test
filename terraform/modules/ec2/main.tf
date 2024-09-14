fb581360d572d677c86ac37a4affa7efc325bbfa
t-kyn-git <>
2024-09-14 22:11:27 +0900

    test 0004

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -27,14 +27,14 @@ data "aws_vpc" "default" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = data.aws_vpc.default.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
 }
 
