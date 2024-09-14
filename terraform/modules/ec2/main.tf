09d62fd06fb2484675c1fe6ebac10ecf94e9abd3
t-kyn-git <>
2024-09-14 23:07:05 +0900

    test 0013

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,3 +1,31 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = element(var.subnet_cidrs, 0)
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet_db1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = element(var.subnet_cidrs, 1)
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "private_subnet_db2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = element(var.subnet_cidrs, 2)
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = element(var.subnet_cidrs, 3)
+  availability_zone = "us-east-1d"
+}
+
 # EC2 インスタンスの作成（パブリックとプライベート）
 
 resource "aws_instance" "public_instance" {
