84657c5bc2c71471524df5957ecc120e3ae4cf84
t-kyn-git <>
2024-09-14 16:55:52 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,1 @@
 # Main Terraform configuration
-output "vpc_id" {
-  value = aws_vpc.main_vpc.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "cloudwatch_alarm_name" {
-  value = aws_cloudwatch_metric_alarm.cpu_alarm.alarm_name
-}
