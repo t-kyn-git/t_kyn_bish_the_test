626889147202c7c289fd27469c2a50f7bcf46594
t-kyn-git <>
2024-09-14 17:44:46 +0900

    fix 003

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,14 +1,14 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main_vpc.id
-}
+#output "vpc_id" {
+#  description = "The ID of the VPC"
+#  value       = aws_vpc.main_vpc.id
+#}
 
-output "subnet_ids" {
-  description = "The IDs of the subnets"
-  value       = [
-    aws_subnet.public_subnet.id,
-    aws_subnet.private_subnet_db1.id,
-    aws_subnet.private_subnet_db2.id,
-    aws_subnet.private_subnet.id
-  ]
-}
+#output "subnet_ids" {
+#  description = "The IDs of the subnets"
+#  value       = [
+#    aws_subnet.public_subnet.id,
+#    aws_subnet.private_subnet_db1.id,
+#    aws_subnet.private_subnet_db2.id,
+#    aws_subnet.private_subnet.id
+#  ]
+#}
