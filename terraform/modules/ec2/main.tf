29f22ac6ab056d2800ba086dc29a0421a8af94a0
t-kyn-git <>
2024-09-14 23:09:17 +0900

    test0014

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -53,20 +53,20 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = var.public_subnet_id
-  #cidr_block        = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
+#resource "aws_subnet" "public_subnet" {
+#  vpc_id                  = data.aws_vpc.default.id
+#  cidr_block              = var.public_subnet_id
+#  #cidr_block        = "10.0.1.0/24"
+#  availability_zone       = "us-east-1a"
+#  map_public_ip_on_launch = true
+#}
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = data.aws_vpc.default.id
-  cidr_block        = var.private_subnet_id
-  #cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-}
+#resource "aws_subnet" "private_subnet" {
+#  vpc_id            = data.aws_vpc.default.id
+#  cidr_block        = var.private_subnet_id
+#  #cidr_block        = "10.0.2.0/24"
+#  availability_zone = "us-east-1a"
+#}
 
 # セキュリティグループの作成（必要に応じて）
 resource "aws_security_group" "my_security_group" {
