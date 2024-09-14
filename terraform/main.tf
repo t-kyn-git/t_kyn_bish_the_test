090dd3668e6546ad0ae65e0b02510422b66fdd13
t-kyn-git <>
2024-09-15 01:12:41 +0900

    test 0031

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,10 @@ module "vpc" {
   source = "./modules/vpc"
 }
 
+module "network" {
+  source = "./modules/network"
+}
+
 #vpc_id      = module.vpc.vpc_id
 module "ec2" {
   source      = "./modules/ec2"
