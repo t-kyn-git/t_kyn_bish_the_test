84efabe80e9bded6b7a294c394592468fe321bf3
t-kyn-git <>
2024-09-14 13:35:07 +0900

    direct commit

--- content/portfolio/portfolio/settings.py
+++ content/portfolio/portfolio/settings.py
@@ -103,9 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
