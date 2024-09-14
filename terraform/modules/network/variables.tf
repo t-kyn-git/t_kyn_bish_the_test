be17b36705467370b9281eec173fe1bd099b55e1
t-kyn-git <>
2024-09-14 22:25:58 +0900

    test 0006

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,9 +1,11 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  type        = string
+  type        = 
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidrs" {
   description = "CIDR blocks for the subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24"]
 }
