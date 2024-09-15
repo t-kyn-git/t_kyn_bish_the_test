2ecf63b4876fc6403c3efce21a3bfcf142390f2b
t-kyn-git <>
2024-09-15 14:17:38 +0900

    20240915 test 0005

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,8 +1,8 @@
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
+#variable "vpc_cidr_block" {
+#  description = "The CIDR block for the VPC"
+#  type        = string
+#  default     = "10.0.0.0/16"
+#}
 
 variable "public_subnet_cidr_block" {
   description = "The CIDR block for the public subnet"
@@ -52,8 +52,8 @@ variable "private_subnet_availability_zone" {
   default     = "us-east-1d"
 }
 
-variable "subnet_cidrs" {
-  description = "CIDR blocks for the subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24"]
-}
+#variable "subnet_cidrs" {
+#  description = "CIDR blocks for the subnets"
+#  type        = list(string)
+#  default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24"]
+#}
