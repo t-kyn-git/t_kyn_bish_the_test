94b31728c334ffacc80ab36c6dee776c9eebc8ee
t-kyn-git <>
2024-09-15 20:49:34 +0900

    20240915 test 0015

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,7 +1,7 @@
-#variable "s3_bucket" {
-#  description = "The name of the S3 bucket to be used by Lambda"
-#  type        = string
-#}
+variable "s3_bucket" {
+  description = "The name of the S3 bucket to be used by Lambda"
+  type        = string
+}
 
 #variable "function_name" {
 #  description = "The name of the Lambda function"
