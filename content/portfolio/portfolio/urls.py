f40582cd0a7e6d34ccb8dea247feefb7654616fa
t_kyn <153400466+t-kyn-git@users.noreply.github.com>
2025-02-28 15:37:59 +0000

    first commit

--- content/portfolio/portfolio/urls.py
+++ content/portfolio/portfolio/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include   # Add
 urlpatterns = [
     path('sample_app/', include('sample_app.urls')),   # Add
     path('admin/', admin.site.urls),
+    path('album/', AlbumView.as_view(), name='album'),
 ]
