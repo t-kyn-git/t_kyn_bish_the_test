897f1908e1a0fefe1e4fa33882c7a5eba6b4aec8
t_kyn <>
2025-08-26 22:00:20 +0900

    update the output network

--- terraform_t_kyn/modules/network/outputs.tf
+++ terraform_t_kyn/modules/network/outputs.tf
@@ -8,11 +8,11 @@ output "public_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
 
-output "private_subnet_id_db1" {
+output "private_subnet_db1_id" {
   value = aws_subnet.private_subnet_db1.id
 }
 
-output "private_subnet_id_db2" {
+output "private_subnet_db2_id" {
   value = aws_subnet.private_subnet_db2.id
 }
 
