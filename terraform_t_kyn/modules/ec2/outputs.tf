74866f0150d5cc1125ef733a5ad51340cf7ae50f
t_kyn <>
2025-08-26 21:46:03 +0900

    update the db tf

--- terraform_t_kyn/modules/ec2/outputs.tf
+++ terraform_t_kyn/modules/ec2/outputs.tf
@@ -5,3 +5,11 @@ output "public_instance_id" {
 output "private_instance_id" {
   value = aws_instance.private_instance.id
 }
+
+output "private_instance_db1_id" {
+  value = aws_instance.private_instance_db1.id
+}
+
+output "private_instance_db2_id" {
+  value = aws_instance.private_instance_db2.id
+}
--- terraform_t_kyn/modules/securitygroup/outputs.tf
+++ terraform_t_kyn/modules/securitygroup/outputs.tf
@@ -2,6 +2,10 @@ output "security_group_id" {
   value = aws_security_group.allow_http_ssh.id
 }
 
-output "mysql_security_group_id" {
-  value = aws_security_group.allow_mysql.id
+output "db_write_security_group_id" {
+  value = aws_security_group.allow_db_write.id
+}
+
+output "db_read_security_group_id" {
+  value = aws_security_group.allow_db_read.id
 }
